a68a0238c4631ad661b09e38c3f17f4ef48fdaea
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-11-28 10:10:47 +0800

    ✨feat: install and setup eslint plugin tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
+  extends: ["expo", "prettier", "plugin:tailwindcss/recommended"],
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": "error",
