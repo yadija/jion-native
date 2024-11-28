817c658cf765be4aac8b2beeecca8b2423d8c9a5
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-11-28 12:50:41 +0800

    ✨feat: make index with dummy data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,9 @@ import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,57 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { cssInterop } from "nativewind";
+
+/**
+ * This is a workaround for a bug with expo-image to work with tailwindcss
+ *
+ * @see https://github.com/nativewind/nativewind/issues/1020
+ */
+cssInterop(Image, { className: "style" });
+
+const data = [
+  {
+    id: 1,
+    title: "Dandadan",
+    imageUrl: "https://cdn.myanimelist.net/images/anime/1584/143719l.jpg",
+  },
+  {
+    id: 2,
+    title: "Blue Lock vs. U-20 Japan",
+    imageUrl: "https://cdn.myanimelist.net/images/anime/1584/144860l.jpg",
+  },
+  {
+    id: 3,
+    title: "Chi. Chikyuu no Undou ni Tsuite",
+    imageUrl: "https://cdn.myanimelist.net/images/anime/1749/145922l.jpg",
+  },
+];
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-[#EDEDED]">
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        ListHeaderComponent={() => (
+          <Text className="p-2 text-2xl text-[#222C32]">Now: Fall 2024</Text>
+        )}
+        renderItem={({ item }) => (
+          <View className="h-[150px] w-full flex-row bg-[#225CB3]">
+            <Image
+              source={item.imageUrl}
+              className="size-full"
+              contentFit="cover"
+              transition={1000}
+            />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <View className="p-2">
+            <Text className="text-2xl">No data found</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
