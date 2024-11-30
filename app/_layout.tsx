4dd13468723ed722f57c86d8eb5b34ac46787c8f
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-11-30 21:55:45 +0800

    ✨feat: install and setup tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // styles
-import "../global.css";
+import "@/global.css";
+
+const client = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={client}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
